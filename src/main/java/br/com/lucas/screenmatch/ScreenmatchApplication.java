9c5803b97bd4d16b4bdb31cee60fb889a2638136
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-07-11 13:01:19 -0300

    Modeling seasons

--- src/main/java/br/com/lucas/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/lucas/screenmatch/ScreenmatchApplication.java
@@ -1,12 +1,16 @@
 package br.com.lucas.screenmatch;
 
 import br.com.lucas.screenmatch.model.DataEpisode;
+import br.com.lucas.screenmatch.model.DataSeason;
 import br.com.lucas.screenmatch.model.DataSeries;
 import br.com.lucas.screenmatch.service.ConsumptionApi;
 import br.com.lucas.screenmatch.service.ConvertsData;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -27,5 +31,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		DataEpisode dataEpisode = convertsData.getData(json, DataEpisode.class);
 		System.out.println(dataEpisode);
 
+		List<DataSeason> seasons = new ArrayList<>();
+
+		for (int i = 1; i <= data.totalSeasons(); i++) {
+			json = consumptionApi.getData("https://www.omdbapi.com/?t=gilmore+girls&season="+i+"&apikey=4d7a8f9e");
+			DataSeason dataSeason = convertsData.getData(json, DataSeason.class);
+			seasons.add(dataSeason);
+		}
+		seasons.forEach(System.out::println);
 	}
 }
